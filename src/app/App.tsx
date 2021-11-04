30c5bda0b50a10d6f80152673dd05bcf1d820722
marwinburesch <marwin@posteo.de>
2021-11-04 10:32:56 +0100

    Fix TS warnings

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,18 +8,22 @@ function App(): JSX.Element {
       <Todo
         title="Polish shoes"
         description="All shoes in the cabinet next to the apartment door"
+        isDone={true}
       />
       <Todo
         title="Sell the old Air Jordans 1"
         description="I never wear them anyway"
+        isDone={false}
       />
       <Todo
         title="Buy new laces"
         description="Colors: Neon green, hotpink and Gouda"
+        isDone={false}
       />
       <Todo
         title="Retie the white sneakers"
         description="I'm tired of falling on my face"
+        isDone={false}
       />
     </div>
   );
