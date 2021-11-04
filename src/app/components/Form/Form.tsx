33563cb6e7667cb5d9d92f6ab1844e0ea6687c0e
phlgr <philipp@gartz.dev>
2021-11-04 16:55:19 +0100

    ✨ Control inputs and complete Form component

--- src/app/components/Form/Form.tsx
+++ src/app/components/Form/Form.tsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Form.module.css';
 
-function Form(): JSX.Element {
+type FormProps = {
+  onSubmit: (todo: {
+    title: string;
+    description: string;
+    isDone: boolean;
+  }) => void;
+};
+
+function Form({ onSubmit }: FormProps): JSX.Element {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    onSubmit({ title, description, isDone: false });
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={(event) => handleSubmit(event)}>
       <label>
-        Title: <input type="text" />
+        Title:{' '}
+        <input
+          type="text"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
       </label>
       <label>
-        Description: <input type="text" />
+        Description:{' '}
+        <input
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+          type="text"
+        />
       </label>
       <input type="submit" />
     </form>
