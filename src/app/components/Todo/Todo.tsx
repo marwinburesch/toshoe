76ef70961fa4ff091274e5fbad33a6a685efcf82
marwinburesch <marwin@posteo.de>
2021-11-04 10:32:43 +0100

    Implement conditional styles based on isDone prop

--- src/app/components/Todo/Todo.tsx
+++ src/app/components/Todo/Todo.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import styles from './Todo.module.css';
 
 type TodoProps = {
+  isDone: boolean;
   title: string;
-  description: string;
+  description?: string;
 };
 
-export default function Todo({ title, description }: TodoProps): JSX.Element {
+export default function Todo({
+  title,
+  description,
+  isDone,
+}: TodoProps): JSX.Element {
+  const conditionalStyles = isDone ? styles.done : '';
+
   return (
-    <article className={styles.todo}>
+    <article className={`${styles.todo} ${conditionalStyles}`}>
       <h2 className={styles.title}>{title}</h2>
       <p>{description}</p>
     </article>
